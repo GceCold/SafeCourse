94c263ecd4c00c01162be22d46f1a13f50a2b1a7
GceCold <736131306@qq.com>
2022-05-26 19:33:22 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# SafeCourse
+
+## 使用方法
+
+环境要求：**jre17+**
+
+前往release页面下载编译好的jar文件，或clone后执行`gradlew shadowJar`
+
+在控制台内输入`java -jar SafeCourse.jar`
+
+使用浏览器打开课程页面并登录，`F12`打开开发人员工具，点击`console`或`控制台`并输入以下带代码回车
+
+```javascript
+data = JSON.parse(localStorage.user)
+console.log(data.userId+','+data.tenantCode+','+data.token)
+```
+
+将输出结果复制到控制台内并回车 等待刷课结束
---
+++ src/main/java/ltd/icecold/Main.java
@@ -0,0 +1,25 @@
+package ltd.icecold;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("浏览器内输入以下指令 获取数据");
+        System.out.println("userData = JSON.parse(localStorage.user)");
+        System.out.println("console.log(userData.userId+','+userData.tenantCode+','+userData.token)");
+        System.out.println("请输入浏览器输出的数据：");
+        String data = scanner.nextLine();
+        String[] split = data.trim().split(",");
+        if (split.length != 3) {
+            System.out.println("输入数据错误！");
+            return;
+        }
+        System.out.println();
+        System.out.println("开始获取数据");
+        WeiBanUser weiBanUser = new WeiBanUser(split[0].trim(), split[1].trim(), split[2].trim());
+        weiBanUser.login().finish();
+
+    }
+}
